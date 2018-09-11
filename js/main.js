7e6c7bc848c8d4a6e482aa4098da1fd91b805cd7
Russell Longstaff <24914431+rllongstaff@users.noreply.github.com>
2018-09-11 15:28:48 +0100

    Added js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,18 @@
+const hourHand = document.querySelector('.hand--hour');
+const minuteHand = document.querySelector('.hand--minute');
+const secondHand = document.querySelector('.hand--second');
 
-console.log('Can you see this'); 
+function setDate() {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    const minutes = now.getMinutes();
+    const minutesDegrees = ((minutes / 60) * 360) + 90;
+    const hours = now.getHours();
+    const hoursDegrees = ((hours / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
